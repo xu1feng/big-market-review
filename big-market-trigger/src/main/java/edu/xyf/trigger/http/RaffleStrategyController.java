4bebe9ebc99c89e933ce2aab5467e3261bb5ac7a
2465549609@qq.com <xuyifeng@xuyifengdeMacBook-Air.local>
2025-09-14 17:02:06 +0800

    feat: 对接联调额度签到权重接口

--- big-market-trigger/src/main/java/edu/xyf/trigger/http/RaffleStrategyController.java
+++ big-market-trigger/src/main/java/edu/xyf/trigger/http/RaffleStrategyController.java
@@ -134,6 +134,7 @@ public class RaffleStrategyController implements IRaffleStrategyService {
         }
     }
 
+    @RequestMapping(value = "query_raffle_strategy_rule_weight", method = RequestMethod.POST)
     @Override
     public Response<List<RaffleStrategyRuleWeightResponseDTO>> queryRaffleStrategyRuleWeight(RaffleStrategyRuleWeightRequestDTO request) {
         try {
